809a0ff5cb24cb291410f69833cb1282abb6bc42
Ghimpusan <37893246+AlexGhimpusan@users.noreply.github.com>
2021-06-23 12:32:34 +0100

    first commit

---
+++ scraper.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+
+URL = 'https://www.amazon.de/EliteDisplay-E233-58-42-inch-Monitor/dp/B079347GWN/ref=sr_1_1_sspa?crid=3A0EZ9R1VA4U7&dchild=1&keywords=monitor+27+zoll&qid=1624376227&sprefix=monitor%2Caps%2C199&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMVVJNlExM0pZMUNJJmVuY3J5cHRlZElkPUEwODE0Njg2M1FYTE5XR05VVVBQJmVuY3J5cHRlZEFkSWQ9QTEwMjQzNzMxNUJCWDJLWlZFWFkyJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
+
+headers = {
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
+
+def checkPrice():
+    page = requests.get(URL, headers = headers)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    title = soup.find(id="productTitle").get_text()
+    price = soup.find(id="priceblock_ourprice").get_text()
+    convertedPrice = float(price[0:3])
+
+    if convertedPrice < 300:
+        sendMail()
+
+def sendMail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+
+    server.starttls()
+    server.ehlo()
+
+    server.login('ghimpusanindiratransexpress@gmail.com', 'alex.smecherul007')
+
+    subject = 'Price fell down!'
+    body = 'Check the amazon link: https://www.amazon.de/EliteDisplay-E233-58-42-inch-Monitor/dp/B079347GWN/ref=sr_1_1_sspa?crid=3A0EZ9R1VA4U7&dchild=1&keywords=monitor+27+zoll&qid=1624376227&sprefix=monitor%2Caps%2C199&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMVVJNlExM0pZMUNJJmVuY3J5cHRlZElkPUEwODE0Njg2M1FYTE5XR05VVVBQJmVuY3J5cHRlZEFkSWQ9QTEwMjQzNzMxNUJCWDJLWlZFWFkyJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
+    msg = f"Subject: {subject} \n\n {body}"
+
+    server.sendmail(
+        'ghimpusanindiratransexpress@gmail.com',
+        'ghimpusan_alexandru@yahoo.com',
+        msg
+    )
+
+    print("EMAIL SENT!")
+
+    server.quit()
+
+
+checkPrice()
